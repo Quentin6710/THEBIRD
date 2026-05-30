d90e81f331bbe907d62bd54e136e4e0dcf9ccfad
Quentin Majda <majdaquentin@gmail.com>
2026-05-30 19:01:51 +0200

    Simple version done

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,9 +6,14 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
         gameObject.name = "PIAF";
     }
 
@@ -16,10 +21,16 @@ public class BirdScript : MonoBehaviour
     void Update()
     {
 
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && birdIsAlive)
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
